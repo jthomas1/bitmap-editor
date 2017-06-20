21aeff099df9d7be3576b5ff1ea236147491c2be
James Thomas <plinkplonk@gmail.com>
2017-06-20 20:54:02 +0100

    Simple argument parsing and validation.

--- app/bitmap-editor.js
+++ app/bitmap-editor.js
@@ -3,29 +3,56 @@
 const fs = require('fs');
 const readline = require('readline');
 
-var BitmapEditor = function () {
-};
+var BitmapEditor = function() {};
 
-BitmapEditor.prototype.buildMatrix = function () {
-
-};
-
-BitmapEditor.prototype.clear = function () {
+BitmapEditor.prototype.buildMatrix = function() {
 
 };
 
-BitmapEditor.prototype.lineHandler = function (line) {
+BitmapEditor.prototype.clear = function() {
+
+};
+
+BitmapEditor.prototype.print = function () {
+ 
+};
+
+BitmapEditor.prototype.parseArgs = function (line) {
+    if (line.length === 0) {
+        throw new Error(`Please provide one of the following commands: I, C, L, V, H, S`);
+    }
+
     line = line.toUpperCase();
-    if (line[0] === 'I') {
-        this.buildMatrix();
+    let args = line.split(' ');
+
+    if (args.length > 5) {
+        throw new Error(`Too many arguments! ${args}`);
     }
 
-    if(line[0] === 'C') {
+    return args;
+
+};
+
+BitmapEditor.prototype.lineHandler = function(line) {
+    line = line.toUpperCase();
+    let args = line.split(' ');
+
+    switch (args[0]) {
+    case 'I':
+        this.buildMatrix(args[1], args[2]);
+        break;
+    case 'C':
         this.clear();
+        break;
+    case 'S':
+        this.print();
+        break;
+    default:
+        throw new Error(`Unrecognised command: ${args[0]}`);
     }
 };
 
-BitmapEditor.prototype.run = function (file) {
+BitmapEditor.prototype.run = function(file) {
     if (!file || !fs.existsSync(file)) {
         console.log('please provide correct file');
         process.exit();
--- spec/bitmap-editor.spec.js
+++ spec/bitmap-editor.spec.js
@@ -1,37 +1,69 @@
+/* global describe beforeEach it spyOn expect */
+
 const BitmapEditor = require('../app/bitmap-editor');
 
-describe('Bitmap editor', function () {
+describe('Bitmap editor', function() {
     let bmp;
 
-    beforeEach(function () {
+    beforeEach(function() {
         bmp = new BitmapEditor();
     });
 
-    describe('Method: lineHandler', function () {
-        it('is case insensitive', function () {
+    describe('Method: lineHandler', function() {
+        it('is case insensitive', function() {
             spyOn(bmp, 'buildMatrix');
             bmp.lineHandler('i');
             expect(bmp.buildMatrix).toHaveBeenCalled();
         });
 
-        it('calls buildMatrix when the line begins with I', function () {
+        it('calls buildMatrix when the line begins with I', function() {
             spyOn(bmp, 'buildMatrix');
-            bmp.lineHandler('I asdlkasd');
-            expect(bmp.buildMatrix).toHaveBeenCalled();
+            bmp.lineHandler('I 5 6');
+            expect(bmp.buildMatrix).toHaveBeenCalledWith('5', '6');
         });
 
-        it('calls clear when the line begins with C', function () {
+        it('calls clear when the line begins with C', function() {
             spyOn(bmp, 'clear');
             bmp.lineHandler('c');
             expect(bmp.clear).toHaveBeenCalled();
         });
 
-        xit('calls print if the line begins with S', function () {
+        it('calls print if the line begins with S', function() {
+            spyOn(bmp, 'print');
+            bmp.lineHandler('S');
+            expect(bmp.print).toHaveBeenCalled();
+        });
 
+        it('throws an error when an unrecognised command is given', function() {
+            expect(() => {
+                bmp.lineHandler('hello');
+            }).toThrow();
         });
     });
 
-    describe('Method: buildMatrix', function () {
+    describe('Method: parseArgs', function () {
+        it('Converts the input to upper case', function () {
+            expect(bmp.parseArgs('acbdfjsdwi')).toEqual(['ACBDFJSDWI']);
+        });
+
+        it('Splits the input string into individual arguments', function () {
+            expect(bmp.parseArgs('a bc')).toEqual(['A', 'BC']);
+        });
+
+        it('throws an error if there are too many arguments', function () {
+            expect( () => {
+                bmp.parseArgs('a b c d e f g h i j k l m n o p');
+            }).toThrow();
+        });
+
+        it('throws an error if there are not enough arguments', function () {
+            expect( () => {
+                bmp.parseArgs('');
+            }).toThrow();
+        });
+    });
+
+    describe('Method: buildMatrix', function() {
 
     });
 });
