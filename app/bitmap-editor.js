20048e7effff9171e6f8e4182b71e4e8a0429a2e
James Thomas <plinkplonk@gmail.com>
2017-06-20 21:57:22 +0100

    Refactored BitmapEditor into an es6 class

--- app/bitmap-editor.js
+++ app/bitmap-editor.js
@@ -3,73 +3,66 @@
 const fs = require('fs');
 const readline = require('readline');
 
-var BitmapEditor = function() {};
 
-BitmapEditor.prototype.buildMatrix = function() {
+class BitmapEditor {
 
-};
+    buildMatrix() {}
 
-BitmapEditor.prototype.clear = function() {
+    clear() {}
 
-};
+    print() {}
 
-BitmapEditor.prototype.print = function () {
- 
-};
-
-BitmapEditor.prototype.parseArgs = function (line) {
-    if (line.length === 0) {
-        throw new Error(`Please provide one of the following commands: I, C, L, V, H, S`);
-    }
-
-    line = line.toUpperCase();
-    let args = line.split(' ');
-
-    if (args.length > 5) {
-        throw new Error(`Too many arguments! ${args}`);
-    }
-
-    return args;
-
-};
-
-BitmapEditor.prototype.lineHandler = function(line) {
-    line = line.toUpperCase();
-    let args = line.split(' ');
-
-    switch (args[0]) {
-    case 'I':
-        this.buildMatrix(args[1], args[2]);
-        break;
-    case 'C':
-        this.clear();
-        break;
-    case 'S':
-        this.print();
-        break;
-    default:
-        throw new Error(`Unrecognised command: ${args[0]}`);
-    }
-};
-
-BitmapEditor.prototype.run = function(file) {
-    if (!file || !fs.existsSync(file)) {
-        console.log('please provide correct file');
-        process.exit();
-    }
-
-    const lineReader = readline.createInterface({
-        input: fs.createReadStream(file)
-    });
-
-    lineReader.on('line', line => {
-        if (line === 'S') {
-            console.log('There is no image');
-        } else {
-            console.log('unrecognised command :(');
+    parseArgs(line) {
+        if (line.length === 0) {
+            throw new Error(`Please provide one of the following commands: I, C, L, V, H, S`);
         }
-    });
-};
 
+        line = line.toUpperCase();
+        let args = line.split(' ');
 
-module.exports = BitmapEditor;
+        if (args.length > 5) {
+            throw new Error(`Too many arguments! ${args}`);
+        }
+
+        return args;
+    }
+
+    lineHandler(line) {
+        let args = this.parseArgs(line);
+
+        switch (args[0]) {
+        case 'I':
+            this.buildMatrix(args[1], args[2]);
+            break;
+        case 'C':
+            this.clear();
+            break;
+        case 'S':
+            this.print();
+            break;
+        default:
+            throw new Error(`Unrecognised command: ${args[0]}`);
+        }
+    }
+
+    run(file) {
+        if (!file || !fs.existsSync(file)) {
+            console.log('please provide correct file');
+            process.exit();
+        }
+
+        const lineReader = readline.createInterface({
+            input: fs.createReadStream(file)
+        });
+
+        lineReader.on('line', line => {
+            if (line === 'S') {
+                console.log('There is no image');
+            } else {
+                console.log('unrecognised command :(');
+            }
+        });
+    }
+}
+
+module.exports.BitmapEditor = BitmapEditor;
--- spec/bitmap-editor.spec.js
+++ spec/bitmap-editor.spec.js
@@ -1,64 +1,70 @@
 /* global describe beforeEach it spyOn expect */
 
-const BitmapEditor = require('../app/bitmap-editor');
+const { BitmapEditor } = require('../app/bitmap-editor');
 
-describe('Bitmap editor', function() {
-    let bmp;
+describe('Class: BitmapEditor', function() {
+    let editor;
 
     beforeEach(function() {
-        bmp = new BitmapEditor();
+        editor = new BitmapEditor();
+    });
+
+    describe('Method: constructor', function () {
+        it('creates a new instance of the BitmapEditor class', function () {
+            expect(editor).toBeDefined();
+        });
     });
 
     describe('Method: lineHandler', function() {
         it('is case insensitive', function() {
-            spyOn(bmp, 'buildMatrix');
-            bmp.lineHandler('i');
-            expect(bmp.buildMatrix).toHaveBeenCalled();
+            spyOn(editor, 'buildMatrix');
+            editor.lineHandler('i');
+            expect(editor.buildMatrix).toHaveBeenCalled();
         });
 
         it('calls buildMatrix when the line begins with I', function() {
-            spyOn(bmp, 'buildMatrix');
-            bmp.lineHandler('I 5 6');
-            expect(bmp.buildMatrix).toHaveBeenCalledWith('5', '6');
+            spyOn(editor, 'buildMatrix');
+            editor.lineHandler('I 5 6');
+            expect(editor.buildMatrix).toHaveBeenCalledWith('5', '6');
         });
 
         it('calls clear when the line begins with C', function() {
-            spyOn(bmp, 'clear');
-            bmp.lineHandler('c');
-            expect(bmp.clear).toHaveBeenCalled();
+            spyOn(editor, 'clear');
+            editor.lineHandler('c');
+            expect(editor.clear).toHaveBeenCalled();
         });
 
         it('calls print if the line begins with S', function() {
-            spyOn(bmp, 'print');
-            bmp.lineHandler('S');
-            expect(bmp.print).toHaveBeenCalled();
+            spyOn(editor, 'print');
+            editor.lineHandler('S');
+            expect(editor.print).toHaveBeenCalled();
         });
 
         it('throws an error when an unrecognised command is given', function() {
             expect(() => {
-                bmp.lineHandler('hello');
+                editor.lineHandler('hello');
             }).toThrow();
         });
     });
 
     describe('Method: parseArgs', function () {
         it('Converts the input to upper case', function () {
-            expect(bmp.parseArgs('acbdfjsdwi')).toEqual(['ACBDFJSDWI']);
+            expect(editor.parseArgs('acbdfjsdwi')).toEqual(['ACBDFJSDWI']);
         });
 
         it('Splits the input string into individual arguments', function () {
-            expect(bmp.parseArgs('a bc')).toEqual(['A', 'BC']);
+            expect(editor.parseArgs('a bc')).toEqual(['A', 'BC']);
         });
 
         it('throws an error if there are too many arguments', function () {
             expect( () => {
-                bmp.parseArgs('a b c d e f g h i j k l m n o p');
+                editor.parseArgs('a b c d e f g h i j k l m n o p');
             }).toThrow();
         });
 
         it('throws an error if there are not enough arguments', function () {
             expect( () => {
-                bmp.parseArgs('');
+                editor.parseArgs('');
             }).toThrow();
         });
     });
